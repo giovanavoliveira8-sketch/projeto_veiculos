955a59c7a52bdf1c8823b0166374f3875ae94259
Giovana <Giovana.v.oliveira8@aluno.senai.br>
2026-06-25 16:54:18 -0300

    Nâo finalizada

--- js/script_veiculos.js
+++ js/script_veiculos.js
@@ -1,2 +1,16 @@
-//LISTAGEM DE VEÍCULOS
+//ARRAY
 
+let form = document.querySelector('#div-form')
+let forms = document.querySelector('#lista-veiculos')
+
+form.addEventListener('submit', (evt) =>{
+    
+    let objFormDados = new FormData(form)
+    let modelo = objFormDados.get('modelo')
+    let marca = objFormDados.get('marca')
+    let placa = objFormDados.get('placa')
+    let anodefabricacao = objFormDados.get('anodefabricacao')
+    let valormercadoveiculo = objFormDados.get('valormercadoveiculo')
+    let tipodecombustivel = objFormDados.get('tipodecombustivel')
+    
+})
